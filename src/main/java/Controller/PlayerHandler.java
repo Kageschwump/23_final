d0025689e847042fe2cf63c99db598bab354a15a
Kageschwump <kasperfalch@live.dk>
2021-01-08 10:58:39 +0100

    oprettet readPlayerByName metode i PlayerHandler klassen

--- src/main/java/Controller/PlayerHandler.java
+++ src/main/java/Controller/PlayerHandler.java
@@ -1,4 +1,27 @@
 package Controller;
 
+import Model.Player;
+
 public class PlayerHandler {
+
+    private Player[] players;
+
+    public PlayerHandler(int amountOfPlayers){
+        players = new Player[amountOfPlayers];
+    }
+
+    public Player readPlayerByName(String name){
+        Player givenPlayer = null;
+
+        for (int i = 0; i < players.length; i++)
+        {
+            if (players[i].getName().equals(name))
+            {
+                givenPlayer = players[i];
+            }
+        }
+        return givenPlayer;
+    }
+
+
 }
--- src/main/java/Model/Player.java
+++ src/main/java/Model/Player.java
@@ -1,9 +1,28 @@
 package Model;
 
+import gui_fields.GUI_Car;
+import gui_fields.GUI_Player;
+
 public class Player {
     private int id;
     private int age;
     private String name;
     private Account account;
+    private GUI_Car car;
+    private GUI_Player player;
+    private int placement;
+
+
+
+    public Player(int id, String name, int age, GUI_Car car, int startBalance) {
+        this.name = name;
+        this.id = id;
+        this.age = age;
+        this.player = new GUI_Player(name, startBalance, car);
+        this.car = car;
+        account = new Account(startBalance);
+    }
+
+    public String getName(){return name;}
 
 }
