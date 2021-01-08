78399ef0eeb891a39cba818d1faf56d3876dcec1
Kageschwump <kasperfalch@live.dk>
2021-01-08 10:44:06 +0100

    lavet Dice og ShuffleCup klasserne

--- src/main/java/Model/Dice.java
+++ src/main/java/Model/Dice.java
@@ -1,8 +1,29 @@
 package Model;
 
 public class Dice {
-    private int faceValue1;
-    private int faceValue2;
+    private int faceValue;
     private final int MAX=6;
+
+    public Dice(int value){
+        faceValue = value;
+    }
+
+    public int roll(){
+        faceValue = (int)(Math.random() * MAX) + 1;
+        return faceValue;
+    }
+
+    public void setFaceValue(int value) {
+        faceValue = value;
+    }
+
+    public int getFaceValue(){
+        return faceValue;
+    }
+
+    public String toString(){
+        String result = "you rolled: " + faceValue;
+        return result;
+    }
 
 }
--- src/main/java/Model/ShuffleCup.java
+++ src/main/java/Model/ShuffleCup.java
@@ -3,5 +3,29 @@ package Model;
 public class ShuffleCup {
     private Dice dice1;
     private Dice dice2;
+    private int value;
+
+    public ShuffleCup(){
+        Dice dice1 = new Dice(3);
+        Dice dice2 = new Dice(3);
+        this.dice1 = dice1;
+        this.dice2 = dice2;
+    }
+
+    public int roll(){
+        dice1.setFaceValue(dice1.roll());
+        dice2.setFaceValue(dice2.roll());
+        value = dice1.getFaceValue() + dice2.getFaceValue();
+        return value;
+    }
+
+    public Dice getDice1() {
+        return dice1;
+    }
+
+    public Dice getDice2() {
+        return dice2;
+    }
+
 
 }
