cd3965e9a07dc03dc07f8e66c66a508492afa81e
harzzek <39692510+harzzek@users.noreply.github.com>
2021-01-09 10:31:56 +0100

    Color som klasse og ikke colorbg

--- src/main/java/Squaretype/PropertySquare.java
+++ src/main/java/Squaretype/PropertySquare.java
@@ -1,8 +1,10 @@
 package Squaretype;
 
+import java.awt.*;
+
 public class PropertySquare {
     private int id;
     private String name;
     private int rent;
-    private colorbg Color;
+    private Color color;
 }
